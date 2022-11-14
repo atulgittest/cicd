fef1ada7a2d348d7a984d9e0a576b791eeff065a
atulgittest <atul.atulbhandari@gmail.com>
2022-11-14 20:56:13 +0530

    now other

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker This is my first Attemp </h2>'
+    return '<h1>Hello from Flask & Docker This is my first Attemp now this port is running under 5000 port</h2>'
 
 
 if __name__ == "__main__":
